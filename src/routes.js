b875b9d1b6ba687fd15613718ccd9656ef4f76a5
Yuchen Ye <fullstackeryuchen@outlook.com>
2020-06-20 00:46:33 +1000

    update pizza routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const customerRoutes = require("./routes/customers");
+
 const orderRoutes = require("./routes/orders");
 const userRoutes = require("./routes/users");
+const pizzaRoutes= require("./routes/pizza");
+
 const authenticationRoutes = require("./routes/authentication");
 const authenticationGuard = require("./middleware/authGuard");
 
@@ -11,6 +14,7 @@ const router = express.Router();
 router.use("/customers", authenticationGuard, customerRoutes);
 router.use("/orders", orderRoutes);
 router.use("/users", userRoutes);
+router.use("/pizza", pizzaRoutes);
 router.use("/authentication", authenticationRoutes);
 
 module.exports = router;
