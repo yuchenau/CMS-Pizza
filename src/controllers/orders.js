dc1029ff68c2c9f8136883ab9bc419c3e1dc48d0
Yuchen Ye <fullstackeryuchen@outlook.com>
2020-06-20 00:26:20 +1000

    prettier

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,77 +1,60 @@
 // Import mongoose Order Model
-const orderModel = require("../models/order");
-const customerModel = require("../models/customer");
+const orderModel = require('../models/order');
+const customerModel = require('../models/customer');
 
 async function addOrder(req, res) {
-  const {
-    quantity,
-    price,
-    note,
-    paymentMethod,
-    orderTime,
-    orderStatus,
-  } = req.body;
-  const order = new orderModel({
-    quantity,
-    price,
-    note,
-    paymentMethod,
-    orderTime,
-    orderStatus,
-  });
-  await order.save();
-  res.status(201).send(order);
+    const {
+        quantity, price, note, paymentMethod, orderTime, orderStatus 
+    } = req.body;
+    const order = new orderModel({
+        quantity, price, note, paymentMethod, orderTime, orderStatus
+    });
+    await order.save();
+    res.status(201).send(order);
 }
 
 async function getOrder(req, res) {
-  const { id } = req.params;
-  // when customer is deleted, populate('customer') will find there is no customer object
-  const order = await orderModel.findById(id).populate("customer");
-  if (!order) {
-    return res.status(404).send("Order not found");
-  }
-  return res.send(order);
+    const { id } = req.params;
+    // when customer is deleted, populate('customer') will find there is no customer object 
+    const order = await orderModel.findById(id).populate('customer');
+    if (!order) {
+        return res.status(404).send('Order not found');
+    }
+    return res.send(order);
 }
 
-async function getAllOrders(req, res) {
-  const orders = await orderModel.find().exec();
-  return res.send(orders);
+async function getAllOrders(req, res){
+    const orders = await orderModel.find().exec();
+    return res.send(orders);
 }
 
 async function updateOrder(req, res) {
-  const { id } = req.params;
-  const {
-    quantity,
-    price,
-    note,
-    paymentMethod,
-    orderTime,
-    orderStatus,
-  } = req.body;
-  const updatedOrder = await orderModel.findByIdAndUpdate(
-    id,
-    { quantity, price, note, paymentMethod, orderTime, orderStatus },
-    { new: true }
-  );
-  if (!updatedOrder) {
-    return res.status(404).send("Order not found");
-  }
-  return res.send(updatedOrder);
+    const { id } = req.params;
+    const {
+        quantity, price, note, paymentMethod, orderTime, orderStatus
+    } = req.body;
+    const updatedOrder = await orderModel.findByIdAndUpdate(id,
+        { quantity, price, note, paymentMethod, orderTime, orderStatus },
+        { new: true},);
+    if (!updatedOrder) {
+        return res.status(404).send('Order not found');
+    }
+    return res.send(updatedOrder);
 }
 
 async function deleteOrder(req, res) {
-  const { id } = req.params;
-  const deletedOrder = await orderModel.findByIdAndDelete(id);
-  if (!deletedOrder) {
-    return res.status(404).send("Order not found");
-  }
-  return res.sendStatus(200);
+    const { id } = req.params;
+    const deletedOrder = await orderModel.findByIdAndDelete(id);
+    if (!deletedOrder) {
+        return res.status(404).send('Order not found');
+    }
+    return res.sendStatus(200);
 }
 
-module.exports = {
-  addOrder,
-  getOrder,
-  getAllOrders,
-  updateOrder,
-  deleteOrder,
-};
+module.exports= {
+    addOrder,
+    getOrder,
+    getAllOrders,
+    updateOrder,
+    deleteOrder
+}
