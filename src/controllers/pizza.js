ae0bc28b5c345bc4b9669dfa409a6a343a987070
Yuchen Ye <fullstackeryuchen@outlook.com>
2020-06-20 00:46:45 +1000

    update pizza routes

---
+++ src/controllers/pizza.js
@@ -0,0 +1,30 @@
+const OrderModel = require('../models/order');
+const PizzaModel = require('../models/pizza');
+
+function addPizza() {
+    const {  } = req.body;
+}
+
+function getPizza() {
+
+}
+
+function getAllPizza() {
+
+}
+
+function updatePizza() {
+
+}
+
+function deletePizza() {
+
+}
+
+module.exports = {
+    addPizza,
+    getPizza,
+    getAllPizza,
+    updatePizza,
+    deletePizza
+}
---
+++ src/routes/pizza.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+    addPizza,
+    getPizza,
+    getAllPizza,
+    updatePizza,
+    deletePizza
+} = require('../controllers/pizza');
+
+const router = express.Router();
+
+router.post('/', addPizza);
+router.delete('/:id', deletePizza);
+router.put('/:id', updatePizza);
+router.get('/:id', getPizza);
+router.get('/', getAllPizza);
+
+module.exports = router;
